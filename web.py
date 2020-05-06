b5e9e02dd4f4ca35d260550277151ed900b2adff
Yaco <yacodiazc@gmail.com>
2020-05-06 02:56:14 -0300

    WIP web.py. Movie to do

--- web.py
+++ web.py
@@ -1,13 +1,93 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import os 
+import sys , subprocess
+import json
+from torrent import Movie
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class web_controller():
+    session = requests.Session()
 
-    def post_login(username, password):
+    def post_login(self,username, password):
+        link_login = "http://192.168.1.6:8080/api/v2/auth/login"
+        data_login = {'username': username, 'password': password}
         
+        post_login = self.session.post(link_login, data=data_login)
+        print(bcolors.UNDERLINE+"LOGIN_POST: "+bcolors.ENDC+post_login.text)
+        #print(bcolors.UNDERLINE+"LOGIN_POST WITHOUT PARSE: "+bcolors.ENDC+str(login_post))
 
-    def get_home_login():
+    def get_default_save_path(self):
+        link_default_save_path = "http://192.168.1.6:8080/api/v2/app/defaultSavePath"
+        get_default_save_path = self.session.get(link_default_save_path)
+        default_save_path = str(get_default_save_path.text)
+        return default_save_path
+    
+    def post_add_torrent(self,movie):
+        movie = Movie()
+        qbit_add_torrent = "http://192.168.1.6:8080/api/v2/torrents/add"
+        data_add_torrent = {'urls': movie.link, 'rename': movie.name}
 
-    def get_table_movies():
+        post_add_torrent = self.session.post(qbit_add_torrent,data= data_add_torrent)
+        print(bcolors.UNDERLINE+"ADD_POST: "+bcolors.ENDC+post_add_torrent.text)
+        #print(bcolors.UNDERLINE+"ADD_POST WITHOUT PARSE: "+bcolors.ENDC+str(add_post))
+    
+    def get_table_movies(self, movie_to_search):
+        search_movie_url = "https://www.1337x.to/category-search/"+movie_to_search+"/Movies/1/"
+        page_search = requests.get(search_movie_url)
+        soup_search = BeautifulSoup(page_search.content, 'html.parser')
+        #Get the rows of a table
+        try:
+            rows_search = soup_search.find_all('tr')
+        except:
+            print(bcolors.WARNING+"Table row not founded"+bcolors.ENDC)
+        for row in rows_search:
+            try:
+                name_column = soup_search.find_all('td', class_ = 'name')
+                seeds_column = soup_search.find_all('td', class_ = 'seeds')
+                leechs_column = soup_search.find_all('td', class_ = 'leeches')
+                size_column = soup_search.find_all('td', class_ = 'size')
+            except:
+                print(bcolors.WARNING+"Table cells not founded"+bcolors.ENDC)
+            
+        link_file = list()
+        name_file = list()
+        seeds_file = list()
+        leechs_file = list()
+        size_file = list()
 
-    def select_table_movie(index):
+        #Parsing only the text of the tag
+        for n in name_column:
+            name_file.append(n.text)
+        for s in seeds_column:
+            seeds_file.append(s.text)
+        for l in leechs_column:
+            leechs_file.append(l.text)
+        for z in size_column:
+            size_file.append(z.text)
+        for a in name_column:
+            for k in a.find_all('a'):
+                if '/torrent' in k.get('href'):
+                        link_file.append(k.get('href'))
+
+        #Main table with data
+        table_files = pd.DataFrame({'Movie: ': name_file, '/t Movie seeds: ' : seeds_file, 'Movie Size: ' : size_file})
+
+        return table_files
+
+
+
 
     
 
